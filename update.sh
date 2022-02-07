f3e073fbcf67647d4e064b655b3123ec223955a0
星外之神 <wszqkzqk@qq.com>
2022-02-07 14:19:26 +0800

    更改仓库名

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 cd `dirname $0`; pwd
 git add *
 git add .gitignore
-git pull git@github.com:wszqkzqk/pkgcloudbuild.git
+git pull git@github.com:wszqkzqk/msys2pkgcloudbuild.git
 git commit -m 'Updated by update.sh'
-git push -u git@github.com:wszqkzqk/pkgcloudbuild.git
+git push -u git@github.com:wszqkzqk/msys2pkgcloudbuild.git
