4592ce8a9058df8a7befa1f68227c2509f4fbd48
星外之神 <wszqkzqk@qq.com>
2022-02-08 16:24:42 +0800

    Updated by update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 cd `dirname $0`; pwd
 git add *
 git add .gitignore
-git pull git@github.com:wszqkzqk/msys2pkgcloudbuild.git
+git pull git@github.com:wszqkzqk/mingwpkgcloudbuild.git
 git commit -m 'Updated by update.sh'
-git push -u git@github.com:wszqkzqk/msys2pkgcloudbuild.git
+git push -u git@github.com:wszqkzqk/mingwpkgcloudbuild.git
